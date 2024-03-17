ce1face35c2e2e2ed8e26c195daf981038956560
efeykaradag <efeyigitkaradag@icloud.com>
2024-03-17 12:17:51 +0100

    first commit

--- tcp server and sockets.py
+++ tcp server and sockets.py
@@ -1,1 +1,22 @@
-askdjaldkjas
+import socket
+
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM
+)
+
+host = socket.gethostbyname()
+port = 444
+
+serversocket.bind((host,port))
+
+serversocket.listen(3)
+
+while True :
+    clientsocket , adress = serversocket.accept()
+
+    print("recieved connection from" % str(adress))
+
+    message = "Hello thank you for connecting to the server" + "\r\n"
+    clientsocket.send(message)
+
+    clientsocket.close()
